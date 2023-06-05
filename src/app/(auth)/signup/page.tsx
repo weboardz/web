44d8e31e54d8f3561f28064c305d3c7cb09b6a2a
samuel <shemlouk@gmail.com>
2023-06-05 00:10:15 -0300

    feat: add sign up page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,51 @@
+import { ArrowLink, Input, InputProps, Submit } from "@/components";
+
 const SignUp = () => {
-  return <main>🏗️ Page Under Construction! 🚧</main>;
+  return (
+    <div className="flex w-3/5 min-w-min max-w-sm flex-col items-center gap-16">
+      <form action="#" className="flex w-full flex-col gap-8 font-alt">
+        <div className="flex flex-col gap-4">
+          {inputFields.map((data, index) => (
+            <Input key={index} {...{ ...data }} />
+          ))}
+        </div>
+
+        <Submit value="Sign Up" />
+      </form>
+
+      <div className="flex gap-2 text-sm text-Alabaster-600">
+        Already have an account?
+        <ArrowLink text="Sign In" href="/signin" />
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
+
+const inputFields: InputProps[] = [
+  {
+    name: "name",
+    icon: "user",
+    placeholder: "name",
+    type: "text",
+  },
+  {
+    name: "email",
+    icon: "mail",
+    placeholder: "email",
+    type: "email",
+  },
+  {
+    name: "password",
+    icon: "lock",
+    placeholder: "password",
+    type: "password",
+  },
+  {
+    name: "confirmPassword",
+    icon: "asterisk",
+    placeholder: "confirm password",
+    type: "password",
+  },
+];
