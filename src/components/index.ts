ade987e9a76c703b5fa31ebaa8e7bcdc32c7636d
samuel <shemlouk@gmail.com>
2023-06-08 01:33:32 -0300

    chore: add toolbox to index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export * from "./ArrowLink";
 export * from "./Logo";
+export * from "./board/ToolBox";
 export * from "./forms/Input";
 export * from "./forms/OAuthButton";
 export * from "./forms/Submit";
