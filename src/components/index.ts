91393a4174157d751e006862713956ac112e5765
samuel <shemlouk@gmail.com>
2023-06-25 18:42:28 -0300

    chore: update index file from components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,5 @@ export * from "./forms/ArrowLink";
 export * from "./forms/Input";
 export * from "./forms/OAuthButton";
 export * from "./forms/Submit";
+export * from "./home/Card";
 export * from "./layout/Logo";
-
